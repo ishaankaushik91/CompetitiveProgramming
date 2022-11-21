326dbdc97a39559ec180c097e3a6d4536f05cfc2
ishaankaushik91 <ishaankaushik91@gmail.com>
2022-11-21 15:46:33 +0530

    CP

---
+++ 164.js
@@ -0,0 +1,44 @@
+let performanceData = [];
+// Assuming 2 servers have already sent the data
+performanceData.push({macA : 0, data : {cpu : "blah blah (Already)", memory : "blah blah (Already)"}});
+performanceData.push({macA : 8, data : {cpu : "blah blah (Already)", memory : "blah blah (Already)"}});
+IncomingFromDogServer(performanceData);
+console.log(performanceData);
+
+// Many DogServers
+function IncomingFromDogServer(array)
+{
+    for (let i = 0; i < 10; i++)
+    {
+        Populate(i, array, {macA : i, data : {cpu : "Foo", memory : "Top G", otherThings : "blah blah"}});
+    }
+}
+
+// Checks if macAddress already there then only change the data for that mac or else put a new object
+function Populate(start, array, socketData)
+{
+    if (start < 10)
+    {
+        let alreadyThere = SearchMacA(socketData.macA, array);
+        if (alreadyThere != -1)
+        {
+            array[alreadyThere].data = socketData.data;
+            return Populate(start + 1, array, socketData);
+        }
+        array.push(socketData);
+        return Populate(start + 1, array, socketData);
+    }
+}
+
+function SearchMacA(macA, array)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        if (array[i].macA == macA)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
